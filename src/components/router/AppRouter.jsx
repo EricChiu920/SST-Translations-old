19f040c9207d10f2a685c01c0271431983612572
IrieY56 <IrieY56@gmail.com>
2019-01-01 21:21:23 -0500

    add proper routing to novels and their chapters, and also created the novel chapter component

--- src/components/router/AppRouter.jsx
+++ src/components/router/AppRouter.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import FrontPage from '../front/FrontPage';
 import NovelSummary from '../novel/summary/NovelSummary';
 import Header from '../header/Header';
+import NovelChapter from '../novel/chapter/NovelChapter';
+import summary from '../../novels/summary';
+import text from '../../novels/text';
 
 const AppRouter = () => (
   <>
     <div className="header"><Header /></div>
     <Switch>
       <Route exact path="/" render={() => <FrontPage />} />
-      <Route path="/novels/1" render={() => <NovelSummary title="abc" />} />
-      <Route path="/novels/2" render={() => <NovelSummary title="bcd" />} />
+      <Route path="/novels/:novel/:chapter" render={() => <NovelChapter text={text} />} />
+      <Route exact path="/novels/:novel" render={() => <NovelSummary summary={summary.alchemist} />} />
+      <Route exact path="/novels/2" render={() => <NovelSummary title="bcd" teaser />} />
+      <Route render={() => <Redirect to="/" />} />
     </Switch>
   </>
 );
---
+++ src/novels/summary.js
@@ -0,0 +1,15 @@
+const summary = {
+  alchemist: {
+    title: 'The Novice Alchemist\'s store,',
+    name: 'alchemist',
+    author: 'Itsuki Mizuho',
+    genre: 'High Fantasy',
+    tags: ['cruel descriptions', 'heart warming', 'female protagonist', 'magic'],
+    time: '268',
+    words: '133,979',
+    rawLink: 'https://ncode.syosetu.com/n3174fc/',
+    teaser: true,
+  },
+};
+
+export default summary;

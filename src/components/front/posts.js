29bd542214d7c5fca96a57f99db124357f00538d
IrieY56 <IrieY56@gmail.com>
2019-01-06 20:15:03 -0500

    created post component and added a new chapter to alchemist

---
+++ src/components/front/posts.js
@@ -0,0 +1,21 @@
+const posts = [{
+  title: 'Hello World',
+  text: `
+    Hi, welcome to my site. I'm a one man team that just started translating Japanese webnovels.
+    I'm also building the website myself so it's going to ugly.
+    To start off I plan to do teasers of a few webnovels to get some practice translating and also which one I like and want to continue with.
+    If there's a novel you want me to continue or do a teaser of you can let me know by joining the discord server in the contact link.
+    You can also let me know about all constructive criticism or suggestions you have.
+  `,
+  link: '/novels/alchemist/1',
+},
+{
+  title: 'Alchemist Chapter 2',
+  text: `
+    Here's chapter two since the prologue was really short and nothing much happened in that chapter, although nothing happens in this one either.
+    I plan to move on and translate something else next.
+  `,
+  link: '/novels/alchemist/2',
+}];
+
+export default posts;

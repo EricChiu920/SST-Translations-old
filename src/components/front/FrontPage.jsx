9a9567fdefdb8c78ca9d7e5a48eaad2e53d3546d
IrieY56 <IrieY56@gmail.com>
2019-01-06 20:15:03 -0500

    created post component and added a new chapter to alchemist

--- src/components/front/FrontPage.jsx
+++ src/components/front/FrontPage.jsx
@@ -1,17 +1,14 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
 import './FrontPage.css';
+import Post from './Post';
+import posts from './posts';
+
 
 const FrontPage = () => (
   <div className="frontPageClass">
-    { content }
+    <Post posts={posts} />
   </div>
 );
-
-const content = `Hi, welcome to my site. I'm a one man team that just started translating Japanese webnovels.
-I'm also building the website myself so it's going to ugly.
-To start off I plan to do teasers of a few webnovels to get some practice translating and also which one I like and want to continue with.
-If there's a novel you want me to continue or do a teaser of you can let me know by joining the discord server in the contact link.
-You can also let me know about all constructive criticism or suggestions you have.`;
 
 export default FrontPage;
